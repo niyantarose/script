cc68763c2656413e0d8e186d1e2051cf6b3c94e6
niyantarose49 <niyantarose2@gmail.com>
2026-05-29 16:03:58 +0900

    perf(taiwan): force immediate Service Worker activation on extension reload using skipWaiting

--- にゃんたろうず商品登録拡張機能/service-worker.js
+++ にゃんたろうず商品登録拡張機能/service-worker.js
@@ -2,3 +2,6 @@ import "./core/mangaUpdatesClient.js";
 import "./core/titleAnalysis.js";
 import "./backgrounds/taiwan.js";
 import "./backgrounds/aladin.js";
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
